81abb8a6d653c071f73474592f0e6ebcbb4df4fa
Giang Nhật Khánh <73809007+NhatKhanhGiang@users.noreply.github.com>
2021-08-29 22:07:08 +0700

    smalls changes

--- src/Data.java
+++ src/Data.java
@@ -62,7 +62,6 @@ public class Data {
     public int getRangeChoice() {
         return this.rangeChoice;
     }
-    
 
     // Read file csv
     // Method to read the entire csv file from the first row to the last row
@@ -120,6 +119,20 @@ public class Data {
         return "";
     }
 
+    //    public boolean checkInput() {
+//        for (int i = 0; i < get_all_rows().size(); i++){
+//            String row = get_all_rows().get(i);
+//            String[] values = row.split(",");
+//            for (int j = 0 ; j < values.length; j++){
+//                if(values[1].equals(getContinent()) && !values[2].equals(getCountry())) return true;
+//                if(!values[1].equals(getContinent()) && !values[2].equals(getCountry())) return true;
+//                if(values[1].equals(getContinent()) && !values[3].equals(getStart_date())) return true;
+//                if(!values[1].equals(getContinent()) && !values[3].equals(getEnd_date())) return true;
+//            }
+//        }
+//        return false;
+//    }
+
     // Display rows for first case: From start date to end date
     public ArrayList<String> addRow1() {
         int fromIndex = getAllRows().indexOf(getStartRow());
@@ -136,12 +149,11 @@ public class Data {
     public ArrayList<String> addRow2() {
         int fromIndex = getAllRows().indexOf(getStartRow());
         ArrayList<String> result = new ArrayList<>();
-        if(rangeChoice == 1) range *= 7;
         if (fromIndex + range > getAllRows().size()){
             System.out.println("Error");
         }
         else {
-            if (checkRowInfo2()){
+            if (checkRowInfo2()) {
                 System.out.println("Error");
             }
             else {
@@ -158,8 +170,7 @@ public class Data {
     public ArrayList<String> addRow3() {
         int toIndex = getAllRows().indexOf(getStopRow());
         ArrayList<String> result = new ArrayList<>();
-        if (rangeChoice == 1) range *= 7;
-        if (toIndex - range < 0){
+        if (toIndex - range < 0) {
             System.out.println("Error");
         } else {
             if (checkRowInfo3()) {
@@ -170,7 +181,6 @@ public class Data {
                     result.add("\n");
                 }
             }
-
         }
         return result;
     }
@@ -180,9 +190,10 @@ public class Data {
     public boolean checkRowInfo2() {
         int fromIndex = getAllRows().indexOf(getStartRow());
         String[] info_first_row = getAllRows().get(fromIndex).split(",");
-        if (fromIndex + range >= getAllRows().size()){
+        if(rangeChoice == 1) range *= 7;
+        if (fromIndex + range >= getAllRows().size()) {
             System.out.println(" ");
-        } else{
+        } else {
             String[] info_last_row = getAllRows().get(fromIndex + range).split(",");
             for (int j = 0; j < info_first_row.length; j++) {
                 for (int k = 0; k < info_last_row.length; k++) {
@@ -192,7 +203,6 @@ public class Data {
                 }
             }
         }
-
         return false;
     }
 
@@ -203,8 +213,8 @@ public class Data {
         String[] infoStopRow = getAllRows().get(idx).split(",");
         String[] infoFirstRow = getAllRows().get(Math.abs(idx - range)).split(",");
         for (int j = 0; j < infoStopRow.length; j++) {
-            for (int k = 0; k < infoStopRow.length; k++){
-                if (!infoFirstRow[1].equals(infoStopRow[1]) || !infoFirstRow[2].equals(infoStopRow[2])){
+            for (int k = 0; k < infoStopRow.length; k++) {
+                if (!infoFirstRow[1].equals(infoStopRow[1]) || !infoFirstRow[2].equals(infoStopRow[2])) {
                     return true;
                 }
             }
@@ -276,15 +286,13 @@ public class Data {
     // Display second case
     public void display2() {
         if (checkRowInfo2()) {
-            System.out.println(" ");
-        }
-        else {
+            System.out.println("Time range not found!");
+        } else {
             if (rangeChoice == 1) {
-                int days = range * 7;
                 if (range <= 0) {
                     System.out.println("Invalid number of weeks!");
                 } else {
-                    System.out.printf("Your continent is %s, your country is %s\nShowing %d week(s) from %s\n", continent, country, days/7, startDate);
+                    System.out.printf("Your continent is %s, your country is %s\nShowing %d week(s) from %s\n", continent, country, range/7, startDate);
                 }
             } else if (rangeChoice == 2) {
                 int days = range;
@@ -294,21 +302,20 @@ public class Data {
                 System.out.printf("Your continent is %s, your country is %s\nShowing %d day(s) from %s\n", continent, country, days, startDate);
             }
         }
-
     }
 
     // Display third case
     public void display3() {
-        if (checkRowInfo3()){
-            System.out.println("Error");
+        if (checkRowInfo3()) {
+            System.out.println("Time range not found!");
         }
         else {
             if (rangeChoice == 1) {
-                int days = range * 7;
+                range *= 7;
                 if (range <= 0) {
                     System.out.println("Invalid number of weeks!");
                 } else {
-                    System.out.printf("Your continent is %s, your country is %s\nShowing %d week(s) to %s\n", continent, country, days/7, endDate);
+                    System.out.printf("Your continent is %s, your country is %s\nShowing %d week(s) to %s\n", continent, country, range/7, endDate);
                 }
             } else if (rangeChoice == 2) {
                 int days = range;
