a87f9519591fb39a7e904216f60610db3c4f0f26
Giang Nhật Khánh <73809007+NhatKhanhGiang@users.noreply.github.com>
2021-09-05 20:23:35 +0700

    final fixes (pt2)

--- src/Display.java
+++ src/Display.java
@@ -3,15 +3,33 @@ import java.util.*;
 public class Display extends Data {
     public static void display() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Choose a display option: ");
+        System.out.println("There are 2 display options: ");
         System.out.println("1. Tabular");
         System.out.println("2. Chart");
+        System.out.print("Please select an option 1-2: ");
+        int displayOption = sc.nextInt();
 
-        int option = sc.nextInt();
-
-        switch(option) {
+        switch(displayOption) {
             case 1 -> Tabular.tabular();
             case 2 -> Chart.chart();
+            default -> {
+                boolean isInvalid = true;
+                while (isInvalid) {
+                    System.out.print("Invalid selection! Please select an option 1-2: ");
+                    displayOption = sc.nextInt();
+
+                    switch (displayOption) {
+                        case 1 -> {
+                            Tabular.tabular();
+                            isInvalid = false;
+                        }
+                        case 2 -> {
+                            Chart.chart();
+                            isInvalid = false;
+                        }
+                    }
+                }
+            }
         }
     }
 }
